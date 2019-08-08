aa4710b8744fca8f165f7fd27c8e572c1932eef0
ADEOSUN ADEWALE <goldenal86@gmail.com>
2019-08-08 01:04:54 +0100

    Initial commit

---
+++ app/src/main/java/com/example/adeosunadewale/travelmantics/DealAdapter.java
@@ -0,0 +1,153 @@
+package com.example.adeosunadewale.travelmantics;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
+
+import org.w3c.dom.Text;
+
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class DealAdapter extends RecyclerView.Adapter<DealAdapter.DealViewHolder>{
+    ArrayList<TravelDeal> deals;
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mDatabaseReference;
+    private ChildEventListener mChildListener;
+    private ImageView imageDeal;
+
+    public DealAdapter() {
+        //FirebaseUtil.openFbReference("traveldeals");
+        mFirebaseDatabase = FirebaseUtil.mFirebaseDatabase;
+        mDatabaseReference = FirebaseUtil.mDatabaseReference;
+        this.deals = FirebaseUtil.mDeals;
+        mChildListener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                TravelDeal td = dataSnapshot.getValue(TravelDeal.class);
+                Log.d("Deal: ", td.getTitle());
+                td.setId(dataSnapshot.getKey());
+                deals.add(td);
+                notifyItemInserted(deals.size()-1);
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        };
+        mDatabaseReference.addChildEventListener(mChildListener);
+    }
+
+    @Override
+    public DealViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        View itemView = LayoutInflater.from(context)
+                .inflate(R.layout.rv_row, parent, false);
+        return new DealViewHolder(itemView);
+
+    }
+
+    @Override
+    public void onBindViewHolder(DealViewHolder holder, int position) {
+        TravelDeal deal = deals.get(position);
+        holder.bind(deal);
+    }
+
+    @Override
+    public int getItemCount() {
+        return deals.size();
+    }
+
+    public class DealViewHolder extends RecyclerView.ViewHolder
+        implements View.OnClickListener {
+        TextView tvTitle;
+        TextView tvDescription;
+        TextView tvPrice;
+
+        public DealViewHolder(View itemView) {
+            super(itemView);
+            tvTitle =  itemView.findViewById(R.id.tvTitle);
+            tvDescription =itemView.findViewById(R.id.tvDescription);
+            tvPrice = itemView.findViewById(R.id.tvPrice);
+            imageDeal = itemView.findViewById(R.id.imageDeal);
+            itemView.setOnClickListener(this);
+        }
+
+        public void bind(TravelDeal deal) {
+            tvTitle.setText(deal.getTitle());
+            tvDescription.setText(deal.getDescription());
+            Locale locale = new Locale("en","NG");
+            NumberFormat fmt = NumberFormat.getCurrencyInstance(locale);
+            Double dble = Double.parseDouble(deal.getPrice());
+            tvPrice.setText(String.valueOf(fmt.format(dble)));
+            //tvPrice.setText(deal.getPrice());
+            showImage(deal.getImageUrl());
+        }
+
+        @Override
+        public void onClick(View view) {
+            int position = getAdapterPosition();
+            Log.d("Click", String.valueOf(position));
+            TravelDeal selectedDeal = deals.get(position);
+            Intent intent = new Intent(view.getContext(), DealActivity.class);
+            intent.putExtra("Deal", selectedDeal);
+            view.getContext().startActivity(intent);
+        }
+
+        private void showImage(String url ) {
+//            final ProgressDialog mdialog = new ProgressDialog(context);
+//            mdialog.setMessage("Please wait");
+//            mdialog.show();
+            if (url != null && url.isEmpty()==false) {
+                Picasso.get()
+                        .load(url)
+                        .fit()
+                        .centerCrop()
+                        .into(imageDeal, new Callback() {
+                            @Override
+                            public void onSuccess() {
+
+                            }
+
+                            @Override
+                            public void onError(Exception e) {
+
+                            }
+                        });
+
+            }
+        }
+    }
+}
